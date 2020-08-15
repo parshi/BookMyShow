68ffecb4b3d99ce7f7e367da674807a5fd71d89b
parshi <parshi.karthik133@gmail.com>
2020-08-15 18:43:19 +0530

    Add files via upload
    Adding Unit tests for other controllers

---
+++ BMSUnitTestProject/BookingUnitTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookMyShow.Controllers;
+using BookMyShow.Models;
+
+namespace BMSUnitTestProject
+{
+    [TestClass]
+    public class BookingUnitTests
+    {
+        [TestMethod]
+        public void CheckAvalibilityTest()
+        {
+            CheckAvalibilityRequest avalibityRequest = new CheckAvalibilityRequest()
+            {
+                MovieName = "KGF",
+                Cinema = "Mega Multiplex",
+                Location = "Marthalli",
+                City = "Banglore",
+                ShowTime = "11:00 AM",
+            };
+            var controller = new BookingController();
+            CheckAvalibilityResponse result = controller.CheckAvalibility(avalibityRequest);
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void CheckAvalibilityTestForWrongMovie()
+        {
+            CheckAvalibilityRequest avalibityRequest = new CheckAvalibilityRequest()
+            {
+                MovieName = "KGF456",
+                Cinema = "Mega Multiplex",
+                Location = "Marthalli",
+                City = "Banglore",
+                ShowTime = "11:00 AM",
+            };
+            var controller = new BookingController();
+            CheckAvalibilityResponse result = controller.CheckAvalibility(avalibityRequest);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void BookingTestPositive()
+        {
+            BookingRequest request = new BookingRequest()
+            {
+                MovieName = "KGF",
+                Cinema = "Mega Multiplex",
+                Location = "Marthalli",
+                City = "Banglore",
+                ShowTime = "11:00 AM",
+                NumberOfTickets = 10,
+                Class = "GoldClass"
+            };
+            var controller = new BookingController();
+            var response = controller.Booking(request);
+            Response expected = new Response { Status = "Success", Message = "Booking Successfull" };
+            Assert.AreEqual(response,expected);
+        }
+
+        [TestMethod]
+        public void BookingTestFroNumberOfTickets()
+        {
+            BookingRequest request = new BookingRequest()
+            {
+                MovieName = "KGF",
+                Cinema = "Mega Multiplex",
+                Location = "Marthalli",
+                City = "Banglore",
+                ShowTime = "11:00 AM",
+                NumberOfTickets = 1000,
+                Class = "GoldClass"
+            };
+            var controller = new BookingController();
+            var response = controller.Booking(request);
+            Response expected = new Response { Status = "Fail", Message = "Select proper seating class" };
+            Assert.AreEqual(response, expected);
+        }
+    }
+}
---
+++ BMSUnitTestProject/LoginUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookMyShow.Controllers;
+using BookMyShow.Models;
+
+namespace BMSUnitTestProject
+{
+    [TestClass]
+    public class LoginUnitTest
+    {
+        [TestMethod]
+        public void LoginTest()
+        {
+            Login loginDetails = new Login();
+            loginDetails.Email = "admin@bookmyshow.com";
+            loginDetails.Password = "admin";
+            var controller = new LoginController();
+            //Response result = controller.UserLogin(loginDetails);
+            //Response expectedResult =new Response { Status = "Success", Message = "Login Successfully" };
+            //Assert.AreEqual<Response>(result,expectedResult);
+        }
+
+        [TestMethod]
+        public void RegisterTest()
+        {
+            Register details = new Register();
+            details.Name = "admin1";
+            details.Email = "admin1@bookmyshow.com";
+            details.Password = "admin1";
+            var controller = new LoginController();
+            Response result = controller.UserRegistration(details);
+            Response expectedResult = new Response { Status = "Success", Message = "Record SuccessFully Saved." };
+            Assert.AreEqual<Response>(result, expectedResult);
+        }
+    }
+}
